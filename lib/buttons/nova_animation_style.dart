b7c043067e21c776de0f2c319a3feaeeb3c9c0cd
Omar Flores <dev@dev.com>
2025-03-10 08:29:14 -0700

    Add NovaAnimationStyle enum for customizable button animations

---
+++ lib/buttons/nova_animation_style.dart
@@ -0,0 +1,14 @@
+/// Animation style for [NovaButton]
+enum NovaAnimationStyle {
+  /// Subtle animations
+  subtle,
+
+  /// Standard level of animations (default)
+  standard,
+
+  /// Dramatic, eye-catching animations
+  dramatic,
+
+  /// No animations
+  none,
+}
