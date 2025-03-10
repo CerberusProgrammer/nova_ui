406611f821d6b80c72af9cfd1b2efed74d424f0d
Omar Flores <dev@dev.com>
2025-03-10 08:29:27 -0700

    Add NovaBorderStyle enum for customizable button border options

---
+++ lib/buttons/nova_border_style.dart
@@ -0,0 +1,17 @@
+/// Border style options for [NovaButton]
+enum NovaBorderStyle {
+  /// Solid border
+  solid,
+
+  /// Dashed border (animated)
+  dashed,
+
+  /// Double border
+  double,
+
+  /// Glowing border
+  glow,
+
+  /// No border
+  none,
+}
